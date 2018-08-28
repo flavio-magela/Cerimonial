619fe6fedf9efd08768e91f5b340eb1ef1b0d3f1
Flavio Magela <flavio.mrsantos@gmail.com>
2018-08-28 15:26:39 -0300

    retirando os espaços do css e corrigindo a função do .js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-$.(function(){
+
   $(".slider").slick({
       dots: true,
       infinite: true,
@@ -7,7 +7,7 @@ $.(function(){
       adaptiveHeight: true
 
   });
-});
+
 // https://code.jquery.com/jquery-1.11.0.min.js
 // https://code.jquery.com/jquery-migrate-1.2.1.min.js
 // https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js
